d84520ce77900360b7ae853b1358c07120397073
JoaoPires <piresjoaovictor09@gmail.com>
2023-09-01 21:39:41 -0300

    adicionei exercicio 2 e 3

--- funcao.js
+++ funcao.js
@@ -1,3 +1,15 @@
-export function funcionar(valorhora, qtndhora) {
-    return valorhora * qtndhora
+function funcionar(valorhora, qtndhora) {
+  return valorhora * qtndhora;
 }
+
+function mediaPesos(pesos) {
+  let soma = 0;
+  for (let i = 0; i < pesos.length; i++) {
+    soma = soma + pesos[i];
+  }
+  const media = soma / pesos.length;
+
+  return media;
+}
+
+module.exports = { funcionar, mediaPesos };
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express')
-const { funcionar } = require('./funcao')
+const { funcionar, mediaPesos } = require('./funcao')
 
 const app = express()
 app.use(express.json())
 const port = 3000
 
-app.post('/api/exercicio2', (req, res) => {
+app.post('/api/FUP2', (req, res) => {
     const result = funcionar(req.body.valorhora, req.body.qtndhora)
     res.status(200).json({
         message: `resultado: ${result}`
     })
+})
+
+app.post('/api/FUP3', (req, res) => {
+    const pesos = mediaPesos(req.body.pesos)
+    res.status(200).json({
+        media: `${pesos}`
+    })
+    
+}
+)
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
 })
